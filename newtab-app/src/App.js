8d43bfb86b61aa3aceb3bd785c4c27225779eb34
Aaron <1kookyw1@gmail.com>
2024-06-21 18:32:25 -0700

    added on load animations

--- newtab-app/src/App.js
+++ newtab-app/src/App.js
@@ -9,14 +9,15 @@ function App() {
     <>
       {/* <Nav className="absolute"></Nav> */}
       <div className="app min-h-screen min-w-screen bg-gradient-to-tl from-blue-400 to-blue-200 flex items-center justify-center font-mono">
-        <div className="max-w-3xl flex flex-col items-center justify-self-center space-y-4 rounded-xl p-8 bg-white/15 shadow-lg">
-          <h1 className="text-white text-7xl ">Welcome back, Air</h1>
-          <Search className=""></Search>
+        {/*  */}
+        <div className="flex opacity-0 animate-fadeinbounceup max-w-3xl flex-col items-center justify-self-center space-y-4 rounded-xl p-8 bg-white/20 shadow-lg">
+          <h1 className="animate-typing overflow-hidden whitespace-nowrap border-solid border-r-8 border-r-white text-white text-7xl font-bold">Welcome back, Air</h1>
+          <div className="animate-fade opacity-0 min-w-full" style={{animationDelay: '0.5s'}}><Search className=""></Search></div>
           <div className="shortcuts flex flex-row space-x-3">
-            <Card></Card>
-            <Card></Card>
-            <Card></Card>
-            <Card></Card>
+            <div className="animate-dropin opacity-0" style={{animationDelay: '0.5s'}}><Card></Card></div>
+            <div className="animate-dropin opacity-0" style={{animationDelay: '0.6s'}}><Card></Card></div>
+            <div className="animate-dropin opacity-0" style={{animationDelay: '0.7s'}}><Card></Card></div>
+            <div className="animate-dropin opacity-0" style={{animationDelay: '0.8s'}}><Card></Card></div>
           </div>
           
         </div>
--- newtab-app/tailwind.config.js
+++ newtab-app/tailwind.config.js
@@ -4,7 +4,68 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      
+			animation: {
+        typing: 'typing 2s steps(20) forwards, blink .7s infinite',
+				fade: 'fadeIn .5s ease-in-out forwards',
+        fadeinbounceup: 'fade-in-bounceup .75s ease-in-out 0.25s 1 forwards',
+        dropin: 'drop-in 0.5s ease-in-out 0.25s 1 forwards',
+      },
+
+			keyframes: {
+				"fadeIn": {
+					from: { opacity: 0 },
+					to: { opacity: 1 },
+				},
+        "fade-in-bounceup": {
+            "0%": {
+                opacity: 0,
+                transform: "translate3d(0%, 100%, 0)",
+            },
+            "43%": {
+                opacity: 0.3,
+                transform: "translate3d(0%, 0%, 0)",
+            },
+            "66%": {
+                opacity: 0.5,
+                transform: "translate3d(0%, 5%, 0)",
+            },
+            "100%": {
+                opacity: 1,
+                transform: "translate3d(0, 0, 0)",
+            },
+        },
+        "drop-in": {
+            "0%": {
+                opacity: 0,
+                transform: "scale(0)",
+                animationTimingFunction: "cubic-bezier(0.34, 1.61, 0.7, 1)",
+            },
+            "100%": {
+                opacity: 1,
+                transform: "scale(1)",
+            },
+        },
+        typing: {
+          "0%": {
+            width: "0%",
+            visibility: "hidden"
+          },
+          "100%": {
+            width: "100%"
+          }  
+        },
+        blink: {
+          "50%": {
+            borderColor: "transparent"
+          },
+          "100%": {
+            borderColor: "white"
+          }  
+        }
+			},
+		},
   },
   plugins: [],
 }
